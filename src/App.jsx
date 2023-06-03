9666493fe6e1b8099c7f0662933364c7213d22cb
chaso13 <chaso13@yahoo.com>
2023-06-03 14:42:16 -0400

    james wants me to push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Home } from './pages'
+import { Home} from './pages';
+import { Footer } from './components';
 
 function App () {
   return (
@@ -8,8 +9,10 @@ function App () {
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
+    <Footer />
     </div>
   </Router>
+
   )
 }
 
--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -4,7 +4,7 @@ import { contactSVG} from '../assets';
 
 const ContactButton = () => {
   return (
-    <div className={`${styles.flexCenter} rounded-full h-[65px] w-[65px] bg-[#3C1D97] fixed bottom-6 right-6 z-10 content-center`}>
+    <div className={`${styles.flexCenter} rounded-full h-[65px] w-[65px] bg-[#3C1D97] hover:bg-purple-700 fixed bottom-6 right-6 z-10 content-center`}>
         <div className='fill-white'>
             <img src={contactSVG}/>
         </div>
