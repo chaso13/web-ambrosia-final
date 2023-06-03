d222a1486fad06294f5d78da5c54f406b485ee0f
James Hunt <jameshunt@Jamess-MacBook-Pro-2.local>
2023-06-03 15:05:00 -0400

    constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -43,3 +43,78 @@ export const socialMedia = [
       link: "https://www.linkedin.com/",
     },
   ];
+
+  export const seoTags = [
+    {
+      id: "home-tags",
+      title: "Web Ambrosia | Home",
+      description: "",
+      canonical: "https://www.webambrosia.com",
+      name: "",
+      type: "",
+      card: ""
+    },
+    {
+      id: "about-tags",
+      title: "Web Ambrosia | About",
+      description: "",
+      canonical: "https://www.webambrosia.com",
+      name: "",
+      type: "",
+      card: ""
+    },
+    {
+      id: "contact-tags",
+      title: "Web Ambrosia | Contact",
+      description: "",
+      canonical: "https://www.webambrosia.com",
+      name: "",
+      type: "",
+      card: ""
+    },
+    {
+      id: "work-tags",
+      title: "Web Ambrosia | Our Work",
+      description: "",
+      canonical: "https://www.webambrosia.com",
+      name: "",
+      type: "",
+      card: ""
+    },
+    {
+      id: "deadly-accurate-tags",
+      title: "Web Ambrosia | Deadly Accurate",
+      description: "",
+      canonical: "https://www.webambrosia.com",
+      name: "",
+      type: "",
+      card: ""
+    },
+    {
+      id: "chaos-coach-tags",
+      title: "Web Ambrosia | Chaos Coach",
+      description: "",
+      canonical: "https://www.webambrosia.com",
+      name: "",
+      type: "",
+      card: ""
+    },
+    {
+      id: "chaos-coach-vip-tags",
+      title: "Web Ambrosia | Chaos Coaching",
+      description: "",
+      canonical: "https://www.webambrosia.com",
+      name: "",
+      type: "",
+      card: ""
+    },
+    {
+      id: "birch-enterprises-tags",
+      title: "Web Ambrosia | Birch Enterprises",
+      description: "",
+      canonical: "https://www.webambrosia.com",
+      name: "",
+      type: "",
+      card: ""
+    }
+  ]
