1a061003433d8498090ddce4d3d28a6b0e252e47
James Hunt <jameshunt@Jamess-MacBook-Pro-2.local>
2023-06-03 14:43:25 -0400

    ?

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
 import styles from '../style';
-import { contactSVG} from '../assets';
+import { contactSVG } from '../assets';
 
 
 const ContactButton = () => {
+  const [toggle, setToggle] = useState(false)
+  const handler = () => {
+    toggle ?
+      setToggle(false) :
+      setToggle(true)
+  }
   return (
-    <div className={`${styles.flexCenter} rounded-full h-[65px] w-[65px] bg-[#3C1D97] fixed bottom-6 right-6 z-10 content-center`}>
-        <div className='fill-white'>
-            <img src={contactSVG}/>
-        </div>
+    <div
+      onClick={handler}
+      className={
+        toggle ?
+          `${styles.flexCenter} cursor-pointer rounded h-[465px] w-[465px] bg-[#000000] fixed bottom-6 right-6 z-10 content-center` :
+          `${styles.flexCenter} cursor-pointer rounded-full h-[65px] w-[65px] bg-[#3C1D97] fixed bottom-6 right-6 z-10 content-center`
+      }
+    >
+      <div className='fill-white'>
+        <img
+        src={contactSVG}
+        className={toggle ? `select-none hidden` : `select-none flex`} />
+        <img
+        src={contactSVG}
+        className={toggle ? `select-none flex` : `select-none hidden`} />
+      </div>
     </div>
   )
 }
